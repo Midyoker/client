f5bd8c8d8bcb7e8da2832b8a29b62077e0080b1b
Mohit <mohitsharma20020720@gmail.com>
2024-03-01 17:28:09 +0530

    Added ui and api support

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CloudAnimation from './CloudAnimation/CloudAnimation'; // Adjust the path as per your project structure
+
+function Header() {
+  return (
+    <header>
+      <CloudAnimation />
+    </header>
+  );
+}
+
+export default Header;
