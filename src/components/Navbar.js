6776a994ca826192a932f51259e2ac4c5e796ab9
Abhi2003shek <azrack11@gmail.com>
2024-03-06 22:12:16 +0530

    Nav bar and header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,19 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h1 className="navbar-heading">TERRAIN RECOGNITION SYSTEM USING DEEP LEARNING</h1>
+    <div class="container">
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Analyze</li>
+            <li>Stats</li>
+            <li>About Us</li>
+        </ul>
     </nav>
+    <div className="navbar">
+      <h1 className="navbar-heading">TERRAIN RECOGNITION SYSTEM USING DEEP LEARNING</h1>
+    </div>
+    </div>
   );
 };
 
