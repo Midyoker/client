af949977d0f2316a3b9442effe5501a12fdf55d7
Mohit <mohitsharma20020720@gmail.com>
2024-03-13 17:51:46 +0530

    Fixed hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
       <h1 className="hero-heading">TERRAIN RECOGNITION SYSTEM USING DEEP LEARNING</h1>
       <div className='hero-box'>
         <div className='hero-image-container'>
-          <img src="./mountain.png" alt="Terrain" className="hero-image" width={20} height={450} />
+          <img src="./mountain.png" alt="Terrain" className="hero-image" width={20} height={420} />
         </div>
         <p className="hero-text">The Terrain Recognition Using Machine Learning project introduces an automated solution for identifying terrains, benefiting autonomous navigation and disaster response. Through rigorous data analysis and model training, it enhances decision-making in dynamic environments. Positioned at the forefront of machine learning and environmental analysis, it sets new industry benchmarks.</p>
       </div>
